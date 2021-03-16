d9ba58d2f747f2bf0b11921e8b34415c54e4b510
Adriannguyen <phuducnguyen108@gmail.com>
2021-03-17 00:08:28 +0700

    Last commit

--- BTVN/BT01/BT01C27.cpp
+++ BTVN/BT01/BT01C27.cpp
@@ -7,8 +7,7 @@ int main(){
     while (n<0){
         cout << "BYE";
         break;
-    }
-    if ( n>=0 && n%5 == 0){
+    } else if ( n>=0 && n%5 == 0){
         cout << n/5;
     } else {
         cout << "-1";
